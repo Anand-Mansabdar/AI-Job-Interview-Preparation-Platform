ab6140f507fafe70a12dc32ac076fe669d9153cf
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-03-01 22:11:29 +0530

    task: Server and Database Setup

---
+++ backend/src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((err) => {
+      console.log("Error connecting to DB", err);
+    });
+};
+
+module.exports = connectDB;
