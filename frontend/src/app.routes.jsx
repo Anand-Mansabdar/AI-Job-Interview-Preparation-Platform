b4b513848cf7d03df17af2d9a87fd61ab2c75433
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-03-03 17:59:47 +0530

    task: Protected Component to allow only logged in users to access the home page.

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/authentication/pages/Login.jsx";
 import Register from "./features/authentication/pages/Register.jsx";
+import Protected from "./features/authentication/components/Protected.jsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home Page</div>,
+    element: (
+      <Protected>
+        <div>Home Page</div>
+      </Protected>
+    ),
   },
   {
     path: "/login",
