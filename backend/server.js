b09b6dbd509f31bcfb62da366f993c8af242b856
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-03-08 11:53:21 +0530

    task: Completed "Generate Interview Report" function.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
-const invokeGeminiAi = require("./src/services/ai.service");
 
 connectDB();
-
-invokeGeminiAi();
 
 app.listen(3000, () => {
   console.log(`Server running on http://localhost:3000`);
--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -6,42 +6,58 @@ const ai = new GoogleGenAI({
   apiKey: process.env.GEMINI_API_KEY,
 });
 
-const invokeGeminiAi = async () => {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: "Hello Gemini! Explain what is an Interview?",
-  });
+// const invokeGeminiAi = async () => {
+//   const response = await ai.models.generateContent({
+//     model: "gemini-2.5-flash",
+//     contents: "Hello Gemini! Explain what is an Interview?",
+//   });
 
-  console.log(response.text);
-};
+//   console.log(response.text);
+// };
 
 const interviewReportSchema = z.object({
-  matchScore: z.number().description("A score between 0 to 100 indicating how well the candidates profile matches the job description"),
+  matchScore: z.number().describe("A score between 0 to 100 indicating how well the candidates profile matches the job description"),
   technicalQuestions: z.array(z.object({
-    question: z.string().description("The technical questions that can be asked by interviewers"),
-    intention: z.string().description("The intention of the interviewer behind asking this question"),
-    answer: z.string().description("How to answer this question, what points to cover, what approach to take etc...")
-  })).description("The technical questions that can be asked in the interview and how to approach them."),
+    question: z.string().describe("The technical questions that can be asked by interviewers"),
+    intention: z.string().describe("The intention of the interviewer behind asking this question"),
+    answer: z.string().describe("How to answer this question, what points to cover, what approach to take etc...")
+  })).describe("The technical questions that can be asked in the interview and how to approach them."),
   behavioralQuestions: z.array(z.object({
-    question: z.string().description("The technical questions that can be asked by interviewers"),
-    intention: z.string().description("The intention of the interviewer behind asking this question"),
-    answer: z.string().description("How to answer this question, what points to cover, what approach to take etc...")
-  })).description("The behavioral questions that can be asked in the interview and how to approach them."),
+    question: z.string().describe("The technical questions that can be asked by interviewers"),
+    intention: z.string().describe("The intention of the interviewer behind asking this question"),
+    answer: z.string().describe("How to answer this question, what points to cover, what approach to take etc...")
+  })).describe("The behavioral questions that can be asked in the interview and how to approach them."),
   skillGaps: z.array(z.object({
-    skill: z.string().description("The skills in which the candidate lacks"),
-    severity: z.enum(["low", "medium", "high"]).description("The severity of the skill gap")
-  })).description("List of all the skill gaps in the candidates profile along with severity"),
+    skill: z.string().describe("The skills in which the candidate lacks"),
+    severity: z.enum(["low", "medium", "high"]).describe("The severity of the skill gap")
+  })).describe("List of all the skill gaps in the candidates profile along with severity"),
   preparationPlan: z.array(z.object({
-    day: z.number().description("The number of days required to be prepared"),
-    focus: z.string().description("The main focus of the current day in the preparation"),
-    tasks: z.array(z.string().description("List of tasks to be completed on the current day"))
-  })).description("A day-wise preparation plan for the candidate to follow in order to get stronger in concepts")
+    day: z.number().describe("The number of days required to be prepared"),
+    focus: z.string().describe("The main focus of the current day in the preparation"),
+    tasks: z.array(z.string().describe("List of tasks to be completed on the current day"))
+  })).describe("A day-wise preparation plan for the candidate to follow in order to get stronger in concepts")
 });
 
 const generateInterviewReport = async ({
   resume,
   selfDescription,
   jobDescription,
-}) => {};
+}) => {
+  const prompt = `Generate an interview report for a candidate with the following details: 
+  Resume: ${resume}
+  Self Description: ${selfDescription},
+  Job Description: ${jobDescription} `
 
-module.exports = invokeGeminiAi;
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: prompt,
+    config: {
+      responseMimeType: "application/json",
+      responseSchema: zodToJsonSchema(interviewReportSchema)
+    }
+  })
+  return JSON.parse(response.text)
+  // console.log(JSON.parse(response.text))
+};
+
+module.exports = generateInterviewReport;
